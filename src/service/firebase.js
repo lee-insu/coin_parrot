cfa78ee5d262775a7bb2e851f4cafda5bc5636b3
lamda <58967660+lee-insu@users.noreply.github.com>
2021-08-13 17:19:17 +0900

    Update firebase.js

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey:process.env.FIREBASE_REACT_APP_API_KEY ,
-    authDomain:process.env.FIREBASE_REACT_APP_AUTH ,
-    databaseURL:process.env.FIREBASE_REACT_APP_DATABASE ,
-    projectId:process.env.FIREBASE_REACT_APP_PROJECT_ID ,
-    storageBucket:process.env.FIREBASE_REACT_APP_STORAGE ,
-    messagingSenderId:process.env.FIREBASE_REACT_APP_MESSAGING ,
-    appId: process.env.FIREBASE_REACT_APP_APP_ID,
-    measurementId:process.env.FIREBASE_REACT_APP_ANY_ID 
+    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_AUTH,
+    databaseURL:process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId:process.env.REACT_APP_FIREBASE_ANY_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
