0fe95f0660e05f2035cf1a8770c2611c238e7eab
lee-insu <carpediem7760@gmail.com>
2021-08-20 17:13:42 +0900

    코인 랜덤뽑기

--- src/components/coin_info/coin_info.jsx
+++ src/components/coin_info/coin_info.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CoinSelect from './coin_select/coin_select';
 
 const CoinInfo = () => {
+
+    const [coins,getCoins] =useState();
+
+    useEffect(()=> {
+        fetch('https://api.upbit.com/v1/market/all')
+        .then(res => res.json())
+        .then(async res => {
+            const list = await res.map(coin => coin);
+            const expectBtc = list.filter(e => {return e.market.indexOf('BTC-')});
+            const expectUsdt = expectBtc.filter(e=> {return e.market.indexOf('USDT-')});
+            getCoins(expectUsdt);
+            
+        })
+    },[])
+
+
+
+
     return (
-        <div>
-            coininfo
-        </div>
+        <>
+        <CoinSelect coins ={coins}/>
+        </>
     );
 };
 
---
+++ src/components/coin_info/coin_select/coin_select.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const CoinSelect = ({coins}) => {
+
+    const [coin,CoinSelect] = useState(null);
+    const [price,getPrice] = useState();
+    const [changeRate,getChangeRate] = useState();
+    const [ready,setReady] = useState(true);
+    const [loading,setLoading] = useState(false);
+    const [result,setResult] =useState(false);
+
+    const CoinSelected = () => {
+
+        const setCoin = () => {
+            setTimeout(()=> {
+                setLoading(false);
+                setResult(true);
+                const selected = coins[Math.floor(Math.random() * coins.length)]
+                CoinSelect(selected.korean_name);
+                getMarket(selected.market);
+            },2000)
+        }
+
+        if(!coin) {
+            setReady(false);
+            setLoading(true);
+            setCoin()
+        }else{
+            setResult(false);
+            setLoading(true);
+            setCoin();
+        }
+    }
+    
+
+    const getMarket = market => {
+        fetch(`https://api.upbit.com/v1/ticker?markets=${market}`)
+        .then(res => res.json())
+        .then(async res => {
+            await res.map(coin => {
+                getPrice(coin.trade_price);
+                getChangeRate((coin.signed_change_rate*100).toFixed(2))
+            });
+        })
+
+    }
+
+
+
+
+
+
+    return (
+        <>
+        
+            {ready ? <img src="#" alt="ready image"/>:null}
+            {loading ? <img src="#" alt="loading image"/>:null}
+            {result ? <img src="#" alt="result image"/>:null}
+            <button onClick={CoinSelected}>{coin === null ? "코인랜덤뽑기":"다시뽑기"}</button>
+            {coin ? 
+              <>
+                <div>{coin}</div>
+                <div>{price}원</div>
+                <div>{changeRate}</div>
+                </>
+            :null}
+        </>
+    );
+};
+
+export default CoinSelect;
